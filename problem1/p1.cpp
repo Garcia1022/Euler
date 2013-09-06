f7fd97dba0be1c1c483b066f1071bdadcd091efb
Garcia1022 <Matthew@Matthews-MacBook-Pro.local>
2013-09-05 21:15:25 -0700

    Built a simple class
    built a simple class as an excercise

--- problem1/p1.cpp
+++ problem1/p1.cpp
@@ -9,6 +9,51 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 
 
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+class P1{
+
+	
+	public:
+		int runningCount;
+		int argument1;
+		int argument2;
+		
+		bool isMultiple(int num);
+	};
+
+     bool P1::isMultiple(int num){
+		if(num % argument1==0 | num % argument2 ==0  )
+			return true;
+		else
+			return false;
+		
+	}	
+
+
+
+
+
+int main (int argc, const char* argv[])
+	{
+
+		
+
+
+
+
+
+
+
+
+	}
+
+
 
 
 
