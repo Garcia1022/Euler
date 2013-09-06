17c0a4cc0590a56e8b83965556934f96bf9dcaa8
Garcia1022 <Matthew@Matthews-MacBook-Pro.local>
2013-09-05 21:42:22 -0700

    more progress

--- problem1/p1.cpp
+++ problem1/p1.cpp
@@ -25,8 +25,14 @@ class P1{
 		int argument2;
 		
 		bool isMultiple(int num);
+		P1();
 	};
-
+	P1::P1()
+	{
+	runningCount=0;
+	argument1=0;
+	argument2=0;	
+	}
      bool P1::isMultiple(int num){
 		if(num % argument1==0 | num % argument2 ==0  )
 			return true;
@@ -41,16 +47,32 @@ class P1{
 
 int main (int argc, const char* argv[])
 	{
-
+		printf("number of args %d\n",argc);
+		//we need to filter out the first argument...
 		
+		if ((argc-1)<2 | (argc-1) >2)
+		{
+			printf("Invalid arguments. Please enter two arguments you want to test against\n");
+		return 0;		
+		}		
+		P1* object= new P1();
+
+		object->argument1= atoi(argv[1]);
+		object->argument2= atoi(argv[2]);
+		
+		//start at 1 since the problem appears to not include 0 in the set of natural numbers
+		for(int i=1; i<1000;i++){
+
+			if(object->isMultiple(i))
+				object->runningCount++;
+
+		}
+
+		printf("count is %d\n",object->runningCount);
 
 
 
-
-
-
-
-
+	return 0;
 	}
 
 
