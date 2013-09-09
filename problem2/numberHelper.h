d91a9d155ba6bbec2cc8c1bcb084883fc85613dd
Garcia1022 <Matthew@Matthews-MacBook-Pro.local>
2013-09-08 21:30:32 -0700

    Trying the second problem
    Adding helper classes

---
+++ problem2/numberHelper.h
@@ -0,0 +1,25 @@
+//Matthew Garcia
+/*
+A class that I will use and expand further as I solve problems.
+
+
+*/
+#ifndef NUMBERHELPER_H
+#define NUMBERHELPER_H
+
+class numberHelper{
+
+
+	public:
+		bool isEven(int num);
+		bool isOdd(int num);
+		int  nthFibNum(int num);
+	
+
+
+
+
+
+};
+
+#endif
